eacd711806cfd4079777e00dc7b37326fdd55067
lallinger <lallinger.patrick@web.de>
2019-06-13 18:44:19 +0200

    minor fix

--- Services/reservation/reservation.go
+++ Services/reservation/reservation.go
@@ -11,12 +11,12 @@ type reservationData struct {
 	showing int32
 	seats   int32
 	booked  bool
-	user string
+	user    string
 }
 
 type Reservation struct {
 	reservations map[int32]*reservationData
-	nextID int32
+	nextID       int32
 }
 
 func getFreeSeats(reservations map[int32]*reservationData, showing int32) int32 {
@@ -32,38 +32,37 @@ func getFreeSeats(reservations map[int32]*reservationData, showing int32) int32 
 		return -1
 	}
 
-	cinema:=""
+	cinema := ""
 	for _, v := range rsp.Data {
 		if v.Id == showing {
-			cinema=v.Cinema
+			cinema = v.Cinema
 			break
 		}
 	}
 
-	cine := proto.NewCinemaService("cinema",service.Client())
-	resp,err:=cine.GetCinemas(context.TODO(),&proto.CinemaRequest{})
+	cine := proto.NewCinemaService("cinema", service.Client())
+	resp, err := cine.GetCinemas(context.TODO(), &proto.CinemaRequest{})
 	if err != nil {
 		fmt.Println(err)
 		return -1
 	}
 	var seats int32
 	for _, v := range resp.Data {
-		if v.Name==cinema {
-			seats=v.RowLength*v.Rows
+		if v.Name == cinema {
+			seats = v.RowLength * v.Rows
 			break
 		}
 	}
 
-
 	for _, v := range reservations {
-		if v.showing==showing && v.booked {
-			seats-=v.seats
+		if v.showing == showing && v.booked {
+			seats -= v.seats
 		}
 	}
 	return seats
 }
 
-func showingExists(showing int32) bool{
+func showingExists(showing int32) bool {
 	service := micro.NewService(micro.Name("reservationRequest"))
 	service.Init()
 	show := proto.NewShowingService("showing", service.Client())
@@ -83,7 +82,7 @@ func showingExists(showing int32) bool{
 	return false
 }
 
-func userExists(name string) bool{
+func userExists(name string) bool {
 	service := micro.NewService(micro.Name("reservationRequest"))
 	service.Init()
 	user := proto.NewUserService("user", service.Client())
@@ -104,9 +103,9 @@ func userExists(name string) bool{
 }
 
 func (me *Reservation) RequestReservation(ctx context.Context, req *proto.ReservationData, rsp *proto.Response) error {
-	if me.reservations==nil {
-		me.reservations=make(map[int32]*reservationData)
-		me.nextID=0
+	if me.reservations == nil {
+		me.reservations = make(map[int32]*reservationData)
+		me.nextID = 0
 	}
 	if !showingExists(req.Showing) {
 		rsp.Success = false
@@ -118,15 +117,15 @@ func (me *Reservation) RequestReservation(ctx context.Context, req *proto.Reserv
 		rsp.Message = fmt.Sprintf("User %s does not exist.", req.User)
 		return nil
 	}
-	if tmp:=getFreeSeats(me.reservations,req.Showing);tmp<req.Seats {
+	if tmp := getFreeSeats(me.reservations, req.Showing); tmp < req.Seats {
 		rsp.Success = false
 		rsp.Message = fmt.Sprintf("Not enough seats. Only %d remaining, trying to reserve %d.", tmp, req.Seats)
 		return nil
 	}
 
-	me.reservations[me.nextID]=&reservationData{seats:req.Seats,showing:req.Showing,booked:false,user:req.User}
-	rsp.Success = false
-	rsp.Message = fmt.Sprintf("Booked %d seats for showing %d. Reservation ID %d.",req.Seats,req.Showing,me.nextID )
+	me.reservations[me.nextID] = &reservationData{seats: req.Seats, showing: req.Showing, booked: false, user: req.User}
+	rsp.Success = true
+	rsp.Message = fmt.Sprintf("Requested %d seats for showing %d. Reservation ID %d.", req.Seats, req.Showing, me.nextID)
 	me.nextID++
 	return nil
 }
@@ -137,15 +136,15 @@ func (me *Reservation) BookReservation(ctx context.Context, req *proto.Reservati
 		return nil
 	}
 
-	if getFreeSeats(me.reservations,me.reservations[req.ReservationID].showing)< me.reservations[req.ReservationID].seats {
+	if getFreeSeats(me.reservations, me.reservations[req.ReservationID].showing) < me.reservations[req.ReservationID].seats {
 		rsp.Success = false
 		rsp.Message = fmt.Sprintf("Could not book reservation ID %d, not enough free seats. Try again later or delete reservation.", req.ReservationID)
 		return nil
 	}
 
-	me.reservations[req.ReservationID].booked=true
+	me.reservations[req.ReservationID].booked = true
 	rsp.Success = true
-	rsp.Message = fmt.Sprintf("Booked %d seats for showing %d.", me.reservations[req.ReservationID].seats,me.reservations[req.ReservationID].showing)
+	rsp.Message = fmt.Sprintf("Booked %d seats for showing %d.", me.reservations[req.ReservationID].seats, me.reservations[req.ReservationID].showing)
 
 	return nil
 }
@@ -157,14 +156,14 @@ func (me *Reservation) DeleteReservation(ctx context.Context, req *proto.Reserva
 	}
 
 	delete(me.reservations, req.ReservationID)
-	rsp.Success=true
-	rsp.Message=fmt.Sprintf("Deleted reservation %d.",req.ReservationID)
+	rsp.Success = true
+	rsp.Message = fmt.Sprintf("Deleted reservation %d.", req.ReservationID)
 
 	return nil
 }
 func (me *Reservation) GetReservations(ctx context.Context, req *proto.ReservationRequest, rsp *proto.ReservationResponse) error {
 	for k, v := range me.reservations {
-		rsp.Data= append(rsp.Data, &proto.ReservationData{ReservationID:k,Seats:v.seats,Showing:v.showing,Booked:v.booked})
+		rsp.Data = append(rsp.Data, &proto.ReservationData{ReservationID: k, Seats: v.seats, Showing: v.showing, Booked: v.booked, User: v.user})
 	}
 	return nil
 }
